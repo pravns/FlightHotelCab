0b72a8b20de57c2839bde81a57fd0b549f625b77
Senthil Kumar <psenthil2022@gmail.com>
2016-06-21 17:20:15 +0530

    Fixed the reset and highlighting again

--- functionality.js
+++ functionality.js
@@ -18,22 +18,31 @@ function find_text() {
 
 
 function flight_available() {
+	var flights = [1,2,3,4,5];
 	var flight_name = ["IndiGo","JetAir","GoAir","Spicejet","AirIndia"];
 	var flight_fare = ["3000 Rs", "4000 Rs", "4800 Rs", "5500 Rs", "2500 Rs"];
+	var li = "";
 	//var rand_flight = flight_name[Math.floor(Math.random() * flight_name.length)];
 	//var rand_fare = flight_fare[Math.floor(Math.random() * flight_fare.length)];
-	$('#fdetails').css('display','block');
-	document.getElementById("fd1").innerHTML= flight_name[0]+" and Fare: "+flight_fare[0];
-	document.getElementById("fd2").innerHTML= flight_name[1]+" and Fare: "+flight_fare[1];
-	document.getElementById("fd3").innerHTML= flight_name[2]+" and Fare: "+flight_fare[2];
-	document.getElementById("fd4").innerHTML= flight_name[3]+" and Fare: "+flight_fare[3];
-	document.getElementById("fd5").innerHTML= flight_name[4]+" and Fare: "+flight_fare[4];
+	// $('#fdetails').css('display','block');
+	for (var i = 0; i < flights.length; i++) {
+		li =  document.createElement('li');
+		li.innerHTML = 	"<div class='fd"+flights[i]+"'>"+flight_name[i]+" and Fare: "+flight_fare[i]+"</div>"
+		document.getElementById("roundedlist").appendChild(li);
+		li = ""
+	};
+
+	// document.getElementById("fd1").innerHTML= flight_name[0]+" and Fare: "+flight_fare[0];
+	// document.getElementById("fd2").innerHTML= flight_name[1]+" and Fare: "+flight_fare[1];
+	// document.getElementById("fd3").innerHTML= flight_name[2]+" and Fare: "+flight_fare[2];
+	// document.getElementById("fd4").innerHTML= flight_name[3]+" and Fare: "+flight_fare[3];
+	// document.getElementById("fd5").innerHTML= flight_name[4]+" and Fare: "+flight_fare[4];
 }
 
 function hotel_available() {
  	var hotel_name = ["Hotel Taj","The Leela Palace","The Taj Mahal","The Oberoi Grand","Umaid Bhawan Palace"];
  	var hotel_fare = ["2000 Rs/day","5000 Rs/day","10000 Rs/day","12000 Rs/day","20000 Rs/day"];
- 	$('#hdetails').css('display','block');
+ 	// $('#hdetails').css('display','block');
  	document.getElementById("hd1").innerHTML= hotel_name[0]+" and Fare: "+hotel_fare[0];
 	document.getElementById("hd2").innerHTML= hotel_name[1]+" and Fare: "+hotel_fare[1];
 	document.getElementById("hd3").innerHTML= hotel_name[2]+" and Fare: "+hotel_fare[2];
@@ -44,7 +53,7 @@ function hotel_available() {
 function cab_available() {
  	var cab_name = ["Taxi for sure","Uber","Ola","Spot city taxi ","Meru cabs","Mega cabs"];
  	var cab_fare = ["20 Rs/km","12 Rs/km","10 Rs/km","15 Rs/km","8 Rs/km","9 Rs/km"];
- 	$('#cdetails').css('display','block');
+ 	// $('#cdetails').css('display','block');
  	document.getElementById("cd1").innerHTML= cab_name[0]+" and Fare: "+cab_fare[0];
 	document.getElementById("cd2").innerHTML= cab_name[1]+" and Fare: "+cab_fare[1];
 	document.getElementById("cd3").innerHTML= cab_name[2]+" and Fare: "+cab_fare[2];
@@ -52,17 +61,20 @@ function cab_available() {
 	document.getElementById("cd5").innerHTML= cab_name[4]+" and Fare: "+cab_fare[4];
 }
 
-function changeColour(element) {
-	if (element.checked) {
-    	document.getElementById("fd1").style.color = "green";
+function changeColour(choice) {
+	var flights = [1,2,3,4,5];
+	for (var i = 0; i < flights.length; i++) {
+		var item = document.getElementsByClassName('fd'+flights[i])
+		item[0].className = item[0].className.replace('active', '')
+		// item.
+	};
+	
+	var selectedItem = document.getElementsByClassName('fd'+choice)
+	selectedItem[0].className = selectedItem[0].className + ' active'
 
-  	}  
-  	else{
-
-  	}
 }
 
-$('#radio-button').on('change', function() {
-   alert($('input[name="radioGroup"]:checked', '#radio-button').val()); 
-   alert($(this).attr('class'));
-}); 
+// $('#radio-button').on('change', function() {
+//    alert($('input[name="radioGroup"]:checked', '#radio-button').val()); 
+//    alert($(this).attr('class'));
+// }); 
